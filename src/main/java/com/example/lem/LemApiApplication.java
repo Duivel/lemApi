8e53b774cfcf78c4efc5ca9329d2b7dba7877430
QuanNL1 <quannl@outlook.com>
2018-02-08 12:48:40 +0900

    Test git

---
+++ src/main/java/com/example/lem/LemApiApplication.java
@@ -0,0 +1,12 @@
+package com.example.lem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LemApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LemApiApplication.class, args);
+	}
+}
